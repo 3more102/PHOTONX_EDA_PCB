2ef7bdc000f4db542b197acaf86dd92718404577
omar islam <s-omar.abdalla@zewailcity.edu.eg>
2026-09-18 15:55:20 +0300

    Prevent pytest from collecting testability score helper

--- src/photonx_eda_pcb/testability_analysis/score.py
+++ src/photonx_eda_pcb/testability_analysis/score.py
@@ -1,3 +1,5 @@
 def testability_score(report):
     coverage=float(report.metrics.get("net_test_coverage",0));exposed=float(report.metrics.get("exposed_fraction",0))
     return round(.75*coverage+.25*exposed,6)
+
+testability_score.__test__ = False
