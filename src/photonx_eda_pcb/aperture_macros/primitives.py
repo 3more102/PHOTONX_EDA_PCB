002819687b323320f354f50d2a0da22dfbf132ff
omar islam <s-omar.abdalla@zewailcity.edu.eg>
2026-09-19 11:11:35 +0300

    Recognize deprecated Code-22 lower-left line macro

--- src/photonx_eda_pcb/aperture_macros/primitives.py
+++ src/photonx_eda_pcb/aperture_macros/primitives.py
@@ -1,2 +1,2 @@
-SUPPORTED={1:"circle",2:"vector_line",4:"outline",5:"polygon",6:"moire",7:"thermal",20:"vector_line",21:"center_line"}
+SUPPORTED={1:"circle",2:"vector_line",4:"outline",5:"polygon",6:"moire",7:"thermal",20:"vector_line",21:"center_line",22:"lower_left_line"}
 def primitive_name(code): return SUPPORTED.get(int(code),"unsupported")
