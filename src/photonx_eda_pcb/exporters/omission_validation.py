80e15241966b2acb2aac93fa01488c1c2c0a4892
omar islam <s-omar.abdalla@zewailcity.edu.eg>
2026-09-19 19:24:38 +0300

    Validate omission reason codes

--- src/photonx_eda_pcb/exporters/omission_validation.py
+++ src/photonx_eda_pcb/exporters/omission_validation.py
@@ -9,8 +9,10 @@ def validate_omission_manifest(data):
     if len(regions)!=len(set(regions)):issues.append("OMISSION_REGION_DUPLICATE_ID")
     if len(routes)!=len(set(routes)):issues.append("OMISSION_ROUTE_DUPLICATE_ID")
     if set(exported)&set(skipped):issues.append("OMISSION_SLOT_BOTH_EXPORTED_AND_SKIPPED")
-    issue_ids={x.get("object_id") for x in data.get("issues",())}
+    manifest_issues=list(data.get("issues",()))
+    issue_ids={x.get("object_id") for x in manifest_issues}
+    issue_pairs={(x.get("object_id"),x.get("code")) for x in manifest_issues}
     if not set(skipped).issubset(issue_ids):issues.append("OMISSION_SKIPPED_SLOT_WITHOUT_REASON")
-    if not set(regions).issubset(issue_ids):issues.append("OMISSION_REGION_WITHOUT_REASON")
-    if not set(routes).issubset(issue_ids):issues.append("OMISSION_ROUTE_WITHOUT_REASON")
+    if any((object_id,"KICAD_COPPER_REGION_UNSUPPORTED") not in issue_pairs for object_id in regions):issues.append("OMISSION_REGION_WITHOUT_REASON")
+    if any((object_id,"KICAD_ARBITRARY_ROUTE_UNSUPPORTED") not in issue_pairs for object_id in routes):issues.append("OMISSION_ROUTE_WITHOUT_REASON")
     return issues
