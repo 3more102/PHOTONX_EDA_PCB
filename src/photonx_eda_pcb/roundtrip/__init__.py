a183948eebe33fa205ce948ffb8f1fb33fcb44ba
omar islam <s-omar.abdalla@zewailcity.edu.eg>
2026-09-19 23:36:45 +0300

    Expose KiCad connectivity round-trip API

--- src/photonx_eda_pcb/roundtrip/__init__.py
+++ src/photonx_eda_pcb/roundtrip/__init__.py
@@ -1,3 +1,13 @@
 from .fingerprint import board_fingerprint
 from .compare import compare_board_models
-__all__=['board_fingerprint','compare_board_models']
+from .kicad_connectivity import (
+    compare_kicad_connectivity,
+    validate_kicad_connectivity_roundtrip,
+)
+
+__all__ = [
+    "board_fingerprint",
+    "compare_board_models",
+    "compare_kicad_connectivity",
+    "validate_kicad_connectivity_roundtrip",
+]
