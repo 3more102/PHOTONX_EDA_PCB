9a566bd6a69328fb04b5468243d969db4f1a609d
omar islam <s-omar.abdalla@zewailcity.edu.eg>
2026-09-19 19:01:34 +0300

    Export native spatial benchmark helpers

--- src/photonx_eda_pcb/benchmark_comparisons/__init__.py
+++ src/photonx_eda_pcb/benchmark_comparisons/__init__.py
@@ -1,4 +1,15 @@
 from .connectivity import benchmark_connectivity_paths
 from .drc import benchmark_clearance_paths
+from .native_spatial import (
+    benchmark_candidate_pair_backends,
+    native_candidate_pair_summary,
+)
 from .summary import comparison_summary
-__all__=["benchmark_connectivity_paths","benchmark_clearance_paths","comparison_summary"]
+
+__all__ = [
+    "benchmark_connectivity_paths",
+    "benchmark_clearance_paths",
+    "benchmark_candidate_pair_backends",
+    "native_candidate_pair_summary",
+    "comparison_summary",
+]
