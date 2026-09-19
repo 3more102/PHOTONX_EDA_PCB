75bf3d0cf2504933208f6122c42de231576e36ac
omar islam <s-omar.abdalla@zewailcity.edu.eg>
2026-09-19 20:10:08 +0300

    Restrict Gerber format digits to ASCII

--- src/photonx_eda_pcb/parsers/gerber_parts/format_spec.py
+++ src/photonx_eda_pcb/parsers/gerber_parts/format_spec.py
@@ -1,6 +1,6 @@
 import re
 from dataclasses import dataclass
-_RE=re.compile(r"%FS(?P<zero>[LT])A?X(?P<xi>\d)(?P<xd>\d)Y(?P<yi>\d)(?P<yd>\d)\*%")
+_RE=re.compile(r"%FS(?P<zero>[LT])A?X(?P<xi>[0-9])(?P<xd>[0-9])Y(?P<yi>[0-9])(?P<yd>[0-9])\*%")
 @dataclass(frozen=True)
 class GerberFormatSpec:
     x_integer:int; x_decimal:int; y_integer:int; y_decimal:int; zero_suppression:str
