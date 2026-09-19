0d275d99ef4057700f21543a6b20ddf045b83316
omar islam <s-omar.abdalla@zewailcity.edu.eg>
2026-09-19 19:04:28 +0300

    Port persistent-index changes: native/include/photonx_native.h

--- native/include/photonx_native.h
+++ native/include/photonx_native.h
@@ -40,6 +40,8 @@ typedef struct photonx_query_match {
     uint32_t point;
 } photonx_query_match;
 
+typedef void* photonx_spatial_index_handle;
+
 enum photonx_native_status {
     PHOTONX_NATIVE_OK = 0,
     PHOTONX_NATIVE_BUFFER_TOO_SMALL = 1,
@@ -66,6 +68,34 @@ PHOTONX_NATIVE_API int photonx_point_radius_candidates(
     const photonx_point_query* queries,
     uint32_t query_count,
     double cell_size,
+    photonx_query_match* out_matches,
+    uint32_t out_capacity,
+    uint32_t* out_count
+);
+
+PHOTONX_NATIVE_API int photonx_spatial_index_create(
+    const photonx_aabb* boxes,
+    uint32_t box_count,
+    double cell_size,
+    photonx_spatial_index_handle* out_handle
+);
+
+PHOTONX_NATIVE_API void photonx_spatial_index_destroy(
+    photonx_spatial_index_handle handle
+);
+
+PHOTONX_NATIVE_API int photonx_spatial_index_candidate_pairs(
+    photonx_spatial_index_handle handle,
+    double tolerance,
+    photonx_pair* out_pairs,
+    uint32_t out_capacity,
+    uint32_t* out_count
+);
+
+PHOTONX_NATIVE_API int photonx_spatial_index_point_radius_candidates(
+    photonx_spatial_index_handle handle,
+    const photonx_point_query* queries,
+    uint32_t query_count,
     photonx_query_match* out_matches,
     uint32_t out_capacity,
     uint32_t* out_count
