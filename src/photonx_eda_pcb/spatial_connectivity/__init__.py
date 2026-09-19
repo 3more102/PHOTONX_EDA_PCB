5405363d64d4c599617181a8652041f3cdbd34a9
omar islam <s-omar.abdalla@zewailcity.edu.eg>
2026-09-19 18:54:24 +0300

    Export batch radius query APIs

--- src/photonx_eda_pcb/spatial_connectivity/__init__.py
+++ src/photonx_eda_pcb/spatial_connectivity/__init__.py
@@ -1,5 +1,6 @@
 from .model import AABB
 from .grid import SpatialHashIndex
 from .pairs import candidate_pairs
+from .points import build_point_index,radius_query,radius_queries
 from .validation import validate_index
-__all__=["AABB","SpatialHashIndex","candidate_pairs","validate_index"]
+__all__=["AABB","SpatialHashIndex","candidate_pairs","build_point_index","radius_query","radius_queries","validate_index"]
