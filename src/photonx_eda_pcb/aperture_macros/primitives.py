1b7efc987e34c455a40313fa3b06b559788db873
omar islam <s-omar.abdalla@zewailcity.edu.eg>
2026-09-19 10:33:23 +0300

    Recognize legacy Code-2 vector-line alias

--- src/photonx_eda_pcb/aperture_macros/primitives.py
+++ src/photonx_eda_pcb/aperture_macros/primitives.py
@@ -1,2 +1,2 @@
-SUPPORTED={1:"circle",4:"outline",5:"polygon",6:"moire",7:"thermal",20:"vector_line",21:"center_line"}
+SUPPORTED={1:"circle",2:"vector_line",4:"outline",5:"polygon",6:"moire",7:"thermal",20:"vector_line",21:"center_line"}
 def primitive_name(code): return SUPPORTED.get(int(code),"unsupported")
