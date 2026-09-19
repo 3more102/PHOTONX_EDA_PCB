69ae110cc4c43a47bfba6c9e89da871720341a81
omar islam <s-omar.abdalla@zewailcity.edu.eg>
2026-09-19 23:40:41 +0300

    Export KiCad zone reader

--- src/photonx_eda_pcb/kicad_reader/__init__.py
+++ src/photonx_eda_pcb/kicad_reader/__init__.py
@@ -1,4 +1,11 @@
 from .sexpr import parse_sexpr
 from .board_reader import read_kicad_board_text
 from .mechanical_slots import read_mechanical_slots
-__all__=["parse_sexpr","read_kicad_board_text","read_mechanical_slots"]
+from .zones import read_zones
+
+__all__ = [
+    "parse_sexpr",
+    "read_kicad_board_text",
+    "read_mechanical_slots",
+    "read_zones",
+]
