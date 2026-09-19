d03294affda84d96505e0aa3144ac8d41e95c9af
omar islam <s-omar.abdalla@zewailcity.edu.eg>
2026-09-19 19:31:41 +0300

    Expose board fingerprint API

--- src/photonx_eda_pcb/__init__.py
+++ src/photonx_eda_pcb/__init__.py
@@ -4,8 +4,10 @@ from .pipeline import reconstruct, ReconstructionResult
 from .project import load_project
 from .validation import validate_board
 from .exporters import export_json, export_kicad, validate_with_kicad_cli
+from .roundtrip import board_fingerprint, board_fingerprint_manifest
 
 __all__ = [
     "ReconstructionConfig", "ReconstructionResult", "reconstruct", "load_project",
     "validate_board", "export_json", "export_kicad", "validate_with_kicad_cli",
+    "board_fingerprint", "board_fingerprint_manifest",
 ]
