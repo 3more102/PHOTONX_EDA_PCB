b3617860f58d490ec72d50ba21e0042657dc5ae0
omar islam <s-omar.abdalla@zewailcity.edu.eg>
2026-09-19 19:04:35 +0300

    Port persistent-index changes: src/photonx_eda_pcb/spatial_connectivity/grid.py

--- src/photonx_eda_pcb/spatial_connectivity/grid.py
+++ src/photonx_eda_pcb/spatial_connectivity/grid.py
@@ -2,7 +2,7 @@ from math import floor
 class SpatialHashIndex:
     def __init__(self,cell_size=1.0):
         if cell_size<=0:raise ValueError("cell_size must be positive")
-        self.cell_size=float(cell_size);self._boxes={};self._cells={}
+        self.cell_size=float(cell_size);self._boxes={};self._cells={};self._revision=0
     def _range(self,a,b):
         return range(floor(a/self.cell_size),floor(b/self.cell_size)+1)
     def insert(self,obj_id,box):
@@ -11,11 +11,14 @@ class SpatialHashIndex:
         self._boxes[oid]=box
         for ix in self._range(box.min_x,box.max_x):
             for iy in self._range(box.min_y,box.max_y):self._cells.setdefault((ix,iy),set()).add(oid)
+        self._revision+=1
     def query(self,box):
         ids=set()
         for ix in self._range(box.min_x,box.max_x):
             for iy in self._range(box.min_y,box.max_y):ids.update(self._cells.get((ix,iy),()))
         return sorted(x for x in ids if self._boxes[x].intersects(box))
+    @property
+    def revision(self):return self._revision
     def box(self,obj_id):return self._boxes[str(obj_id)]
     def ids(self):return tuple(sorted(self._boxes))
     def boxes(self):return {k:self._boxes[k] for k in sorted(self._boxes)}
