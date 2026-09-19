4a3e25a35f1bbca4c518fafc5b0c4f32ca346028
omar islam <s-omar.abdalla@zewailcity.edu.eg>
2026-09-19 20:07:21 +0300

    Cover all physical objects in ID checks

--- src/photonx_eda_pcb/checks/ids.py
+++ src/photonx_eda_pcb/checks/ids.py
@@ -1,5 +1,7 @@
 from .base import CheckIssue
+from ..core.board_objects import iter_physical_objects
 from ..core.collections import duplicates
+
 def check_unique_object_ids(board):
-    ids=[o.id for o in [*board.tracks,*board.pads,*board.drills,*board.outline]]
+    ids=[o.id for o in iter_physical_objects(board)]
     return [CheckIssue("error","DUPLICATE_OBJECT_ID",f"duplicate object id {x}",x) for x in duplicates(ids)]
