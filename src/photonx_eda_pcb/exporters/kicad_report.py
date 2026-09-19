a33d4540d935c73adc938dba4b98fd8407170e1f
omar islam <s-omar.abdalla@zewailcity.edu.eg>
2026-09-19 12:04:31 +0300

    Track omitted regions in KiCad report

--- src/photonx_eda_pcb/exporters/kicad_report.py
+++ src/photonx_eda_pcb/exporters/kicad_report.py
@@ -9,10 +9,12 @@ class KicadExportIssue:
 class KicadExportReport:
     exported_slots:int=0
     skipped_slots:int=0
+    skipped_regions:int=0
     exported_npth_slots:int=0
     exported_plated_slots:int=0
     issues:list[KicadExportIssue]=field(default_factory=list)
     exported_slot_ids:list[str]=field(default_factory=list)
     skipped_slot_ids:list[str]=field(default_factory=list)
+    skipped_region_ids:list[str]=field(default_factory=list)
     @property
     def ok(self):return not any(x.severity=="error" for x in self.issues)
