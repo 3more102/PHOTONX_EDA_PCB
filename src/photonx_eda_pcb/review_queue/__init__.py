9a4f050d220de0beaaf118d49a57226097eef707
omar islam <s-omar.abdalla@zewailcity.edu.eg>
2026-09-19 22:04:43 +0300

    Rebase GUI review queue: src/photonx_eda_pcb/review_queue/__init__.py

--- src/photonx_eda_pcb/review_queue/__init__.py
+++ src/photonx_eda_pcb/review_queue/__init__.py
@@ -1,3 +1,5 @@
+from .from_board import build_board_review_queue
 from .item import ReviewItem
 from .queue import ReviewQueue
-__all__=["ReviewItem","ReviewQueue"]
+
+__all__ = ["ReviewItem", "ReviewQueue", "build_board_review_queue"]
