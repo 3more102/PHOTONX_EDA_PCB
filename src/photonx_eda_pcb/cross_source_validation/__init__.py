b715a62837cc9eeafc4c5066a169838744781a90
omar islam <s-omar.abdalla@zewailcity.edu.eg>
2026-09-19 22:35:01 +0300

    Export cross-source net label comparison

--- src/photonx_eda_pcb/cross_source_validation/__init__.py
+++ src/photonx_eda_pcb/cross_source_validation/__init__.py
@@ -1,4 +1,11 @@
 from .model import SourceObservation
 from .merge import merge_observations
 from .conflicts import find_conflicts
-__all__=['SourceObservation','merge_observations','find_conflicts']
+from .net_validation import compare_net_labels
+
+__all__ = [
+    "SourceObservation",
+    "merge_observations",
+    "find_conflicts",
+    "compare_net_labels",
+]
