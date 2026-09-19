50b93204f4854cc7d47f756f397b2b0cc4417620
omar islam <s-omar.abdalla@zewailcity.edu.eg>
2026-09-19 12:07:29 +0300

    Keep permissive conformance fail-closed

--- tests/test_parser_conformance_permissive_phase29.py
+++ tests/test_parser_conformance_permissive_phase29.py
@@ -1,5 +1,12 @@
 from photonx_eda_pcb.parser_conformance import ConformanceCase,ConformanceExpectation,run_conformance_case
+
 def test_permissive_unsupported_is_diagnostic():
-    c=ConformanceCase("region","gerber","%FSLAX24Y24*%\n%MOMM*%\nG36*\nM02*\n",ConformanceExpectation({"tracks":0},("UNSUPPORTED_GERBER_CONSTRUCT",)),strict=False)
+    c=ConformanceCase(
+        "aperture-block",
+        "gerber",
+        "%FSLAX24Y24*%\n%MOMM*%\n%ABD10*%\n%AB*%\nM02*\n",
+        ConformanceExpectation({"tracks":0},("UNSUPPORTED_GERBER_CONSTRUCT",)),
+        strict=False,
+    )
     r=run_conformance_case(c)
     assert r.passed and "UNSUPPORTED_GERBER_CONSTRUCT" in r.diagnostics
