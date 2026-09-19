843df0907b94a07839b9616e92dbe2a5e50728df
omar islam <s-omar.abdalla@zewailcity.edu.eg>
2026-09-19 11:33:34 +0300

    Align legacy transform regressions with supported OF semantics

--- tests/test_legacy_gerber_transform_safety.py
+++ tests/test_legacy_gerber_transform_safety.py
@@ -23,7 +23,6 @@ def _write(tmp_path: Path, body: str) -> Path:
 @pytest.mark.parametrize(
     "command",
     [
-        "%OFA1B0*%",
         "%ASAYBX*%",
         "%IPNEG*%",
     ],
@@ -76,7 +75,7 @@ def test_non_default_legacy_transform_suppresses_permissive_geometry(
 ):
     path = _write(
         tmp_path,
-        "%OFA1B0*%\n"
+        "%ASAYBX*%\n"
         "X000000Y000000D02*\n"
         "X010000Y000000D01*\n",
     )
