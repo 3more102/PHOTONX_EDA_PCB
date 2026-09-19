0e7cfa36e7b7a459fa57c11f9921a67baa26ece1
omar islam <s-omar.abdalla@zewailcity.edu.eg>
2026-09-19 19:19:14 +0300

    Block trailing Excellon data after M30

--- src/photonx_eda_pcb/preflight.py
+++ src/photonx_eda_pcb/preflight.py
@@ -82,6 +82,7 @@ def _is_strict_blocker(code: str) -> bool:
             "EXCELLON_ROUTE_UNTERMINATED",
             "EXCELLON_ROUTE_ARC_INVALID",
             "EXCELLON_UNITS_UNDECLARED",
+            "EXCELLON_TRAILING_DATA_AFTER_M30",
         }
     )
 
