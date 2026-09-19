eb8f48cf5134282641e0af5d5b6e2168dff44ddf
omar islam <s-omar.abdalla@zewailcity.edu.eg>
2026-09-19 19:32:37 +0300

    Expose board JSON diff helpers

--- src/photonx_eda_pcb/board_diff/__init__.py
+++ src/photonx_eda_pcb/board_diff/__init__.py
@@ -1,4 +1,20 @@
 from .model import DiffEntry, BoardDiff
 from .engine import diff_collections
 from .summary import diff_summary
-__all__=['DiffEntry','BoardDiff','diff_collections','diff_summary']
+from .json_diff import (
+    compact_diff_report,
+    diff_board_paths,
+    diff_board_payloads,
+    load_board_payload,
+)
+
+__all__ = [
+    "DiffEntry",
+    "BoardDiff",
+    "diff_collections",
+    "diff_summary",
+    "compact_diff_report",
+    "diff_board_paths",
+    "diff_board_payloads",
+    "load_board_payload",
+]
