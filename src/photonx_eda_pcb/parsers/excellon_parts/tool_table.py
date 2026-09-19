743487efc8980db495cba315c2e9d9de39d08f19
omar islam <s-omar.abdalla@zewailcity.edu.eg>
2026-09-19 21:06:23 +0300

    Align helper tool parser with ASCII identifiers

--- src/photonx_eda_pcb/parsers/excellon_parts/tool_table.py
+++ src/photonx_eda_pcb/parsers/excellon_parts/tool_table.py
@@ -1,7 +1,7 @@
 import re
 from dataclasses import dataclass
 
-from ...excellon_numeric import UNSIGNED_DECIMAL_PATTERN
+from ...excellon_numeric import TOOL_NUMBER_PATTERN, UNSIGNED_DECIMAL_PATTERN
 
 _RE = re.compile(rf"T(?P<tool>\d+)(?:C(?P<diam>{UNSIGNED_DECIMAL_PATTERN}))?")
 
