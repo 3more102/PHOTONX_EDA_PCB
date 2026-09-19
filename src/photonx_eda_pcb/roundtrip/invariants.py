553c42a98e6ad68e6489e6979ba64f0b16ffd444
omar islam <s-omar.abdalla@zewailcity.edu.eg>
2026-09-19 12:04:24 +0300

    Track region roundtrip invariant

--- src/photonx_eda_pcb/roundtrip/invariants.py
+++ src/photonx_eda_pcb/roundtrip/invariants.py
@@ -1,5 +1,5 @@
 def board_invariants(board):
-    return {"tracks":len(board.tracks),"pads":len(board.pads),"drills":len(board.drills),"slots":len(getattr(board,"slots",())),"nets":len(board.nets),"components":len(board.components),"outline":len(board.outline)}
+    return {"tracks":len(board.tracks),"pads":len(board.pads),"drills":len(board.drills),"slots":len(getattr(board,"slots",())),"regions":len(getattr(board,"regions",())),"nets":len(board.nets),"components":len(board.components),"outline":len(board.outline)}
 def compare_invariants(a,b):
     aa,bb=board_invariants(a),board_invariants(b)
     return {k:(aa[k],bb[k]) for k in aa if aa[k]!=bb[k]}
