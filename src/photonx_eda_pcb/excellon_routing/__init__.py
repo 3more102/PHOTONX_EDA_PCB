081ab4a5407b18f6c9291203e78dddd46a217c71
omar islam <s-omar.abdalla@zewailcity.edu.eg>
2026-09-20 13:36:37 +0300

    Expose exact NPTH route export helpers

--- src/photonx_eda_pcb/excellon_routing/__init__.py
+++ src/photonx_eda_pcb/excellon_routing/__init__.py
@@ -1,5 +1,5 @@
 from .model import RoutedPath,RouteSegment
 from .geometry import route_shape
 from .validation import validate_route
-from .readiness import assess_route_export_readiness
-__all__=["RoutedPath","RouteSegment","route_shape","validate_route","assess_route_export_readiness"]
+from .readiness import assess_route_export_readiness,is_exact_npth_slot_route,route_export_descriptor
+__all__=["RoutedPath","RouteSegment","route_shape","validate_route","assess_route_export_readiness","is_exact_npth_slot_route","route_export_descriptor"]
