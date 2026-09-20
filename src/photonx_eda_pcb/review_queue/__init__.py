8d922de0ed4bcf9ec456c28def5d28acb81cb88b
omar islam <s-omar.abdalla@zewailcity.edu.eg>
2026-09-20 18:18:20 +0300

    Export canonical via evidence review helper

--- src/photonx_eda_pcb/review_queue/__init__.py
+++ src/photonx_eda_pcb/review_queue/__init__.py
@@ -1,5 +1,6 @@
 from .from_board import build_board_review_queue
 from .item import ReviewItem
 from .queue import ReviewQueue
+from .via_evidence import build_via_evidence_rows
 
-__all__ = ["ReviewItem", "ReviewQueue", "build_board_review_queue"]
+__all__ = ["ReviewItem", "ReviewQueue", "build_board_review_queue", "build_via_evidence_rows"]
