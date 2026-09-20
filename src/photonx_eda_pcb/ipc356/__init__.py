d58096dc7246a60007503fe33c4e34297d0b26e5
omar islam <s-omar.abdalla@zewailcity.edu.eg>
2026-09-20 19:01:18 +0300

    Export IPC-356 alignment API

--- src/photonx_eda_pcb/ipc356/__init__.py
+++ src/photonx_eda_pcb/ipc356/__init__.py
@@ -1,3 +1,12 @@
+from .alignment import Ipc356TranslationAlignment, infer_translation_alignment, translate_records
 from .model import Ipc356Record
 from .parser import parse_ipc356, parse_record
-__all__=["Ipc356Record","parse_ipc356","parse_record"]
+
+__all__=[
+    "Ipc356Record",
+    "Ipc356TranslationAlignment",
+    "infer_translation_alignment",
+    "translate_records",
+    "parse_ipc356",
+    "parse_record",
+]
