72e4700f0021277176d982e146154f28aedc014d
omar islam <s-omar.abdalla@zewailcity.edu.eg>
2026-09-20 13:32:56 +0300

    Preserve full proven via span layer path

--- src/photonx_eda_pcb/via_span/model.py
+++ src/photonx_eda_pcb/via_span/model.py
@@ -9,6 +9,9 @@ class ViaSpanCandidate:
     evidence: list[str] = field(default_factory=list)
     proven: bool = False
     pad_ids: tuple[str, ...] = ()
+    layer_names: tuple[str, ...] = ()
 
     def layers(self):
+        if self.layer_names:
+            return self.layer_names
         return tuple(x for x in (self.from_layer, self.to_layer) if x is not None)
